804c4b82d98fdc4de342834792db175e1dd627cf
unknown <steve.potts@bowlereggs.co.uk>
2021-10-19 22:39:00 +0100

    added cdk dragdrop

---
+++ src/app/kanban/board/board.component.ts
@@ -0,0 +1,24 @@
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component, Input, OnInit } from '@angular/core';
+import { Board } from '../board.model';
+import { BoardService } from '../board.service';
+
+@Component({
+  selector: 'app-board',
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss']
+})
+export class BoardComponent implements OnInit {
+  @Input() board: any;
+
+  constructor(private boardService: BoardService) { }
+
+  ngOnInit(): void {
+  }
+
+  taskDrop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.board.tasks, event.previousIndex, event.currentIndex);
+    this.boardService.updateTasks(this.board.id, this.board.tasks);
+  }
+
+}
